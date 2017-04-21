5a61aecf3632ccc4217722ce4ed94ed34baded31
Jonathan Sawyer <jonmsawyer@gmail.com>
2017-04-21 12:30:07 -0800

    Adding 00-hello-world exercise

---
+++ 00-hello-world/HelloWorld.java
@@ -0,0 +1,27 @@
+/**************************************************************************************************
+ * File: HellowWorld.java
+ *
+ * Print the string "Hello World!" onto the console.
+ *
+ * Your assignment: get this to compile and run on your computer. A windows batch file is included
+ * for you in this exercise. It should be enough to run it via:
+ *
+ * C:\learning-java\00-hello-world> run.bat
+ *
+ * And it should output:
+ *
+ * Hello World!
+ *
+ * How I will be "grading" your work
+ * ---------------------------------
+ *
+ * I won't be grading this exercise.
+ */
+
+public class HelloWorld {
+
+    public static void main(String []args) {
+        System.out.println("Hello World!"); // prints Hello World
+    }
+
+}
