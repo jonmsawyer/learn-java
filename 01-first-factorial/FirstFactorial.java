7c411e8d6f25f1d8ad11f7d2f9ea136b8f14efe7
UselessEndeavors <uselessendeavors101@gmail.com>
2017-04-27 16:33:28 -0800

    I added the answer I discovered for first factorial and added it to the top
    It now reflects my solution, but contains a possible bug.
    Signed-off-by: UselessEndeavors <uselessendeavors101@gmail.com>

--- 01-first-factorial/FirstFactorial.java
+++ 01-first-factorial/FirstFactorial.java
@@ -1,3 +1,25 @@
+
+
+import java.util.Scanner;
+
+        public class FirstFactorial {
+
+            private static int factorial(int n) {
+		if (n == 1) return 1;
+		else
+                return n * factorial(n-1);             }
+                public static void main(String[] args) {
+                Scanner s = new Scanner(System.in);
+                System.out.println("Input an integer, and we'll compute its factorial:");
+		int n = s.nextInt();
+		System.out.println(factorial(n));
+		;
+		s.close();
+                
+            }
+     }
+
+
 /**************************************************************************************************
  * File: FirstFactorial.java
  *
